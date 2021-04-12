3903a67d97469a252912363ec6b9c661f7e5672e
Kevin Toro <kevinandrestg7@gmail.com>
2021-04-12 10:40:58 -0500

    se agregó el contenido del aside, y el footer de la página

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ function loadInfo() {
   fetch('https://demo6497253.mockable.io/noticias')
       .then(response => response.json())
       .then(data => loadNews(data));
+
+  fetch('https://demo6497253.mockable.io/categoria/deporte')
+      .then(response => response.json())
+      .then(data => deportesContent(data));
+
+  fetch('https://demo6497253.mockable.io/categoria/tecnologia')
+      .then(response => response.json())
+      .then(data => tecnologiaContent(data));
 }
 
 function loadNews(data) {
@@ -40,6 +48,29 @@ function loadNews(data) {
     if (i === 2) {
       break;
     }
+  }
+}
+
+function deportesContent(data) {
+  const deportes = document.getElementById('deportes-ul');
+  for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.textContent = data[i].titulo;
+    deportes.appendChild(li);
+    if (i === 2){
+      break;
+    }
+  }
+}
+
+function tecnologiaContent(data) {
+  const tecnologia = document.getElementById('tecnologia-ul');
+  for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.textContent = data[i].titulo;
+    tecnologia.appendChild(li);
   }
 }
 
